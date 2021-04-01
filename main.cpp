8f488eef1f8205f99ba12f232985f31bc1545692
MarcoSolarino <marcosolarino@live.it>
2021-04-01 09:22:32 +0200

    - added time evaluation in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include "LocalBinaryPattern.h"
+#include <chrono>
 
 using namespace cv;
 using namespace std;
@@ -8,7 +9,11 @@ int main() {
 
     Mat inputImg = imread("../input/img.jpg", 0);
     imshow("Image before LBP", inputImg);
+    chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     Mat outputImg = localBinaryPattern(inputImg);
+    chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    long time = chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    cout << time << " milliseconds\n";
     imshow("Image after LBP", outputImg);
     waitKey(0);
 
