0860a7109a235d562efc9f279a05d8be4f234384
Simone <simone.pezzulla@stud.unifi.it>
2021-04-05 09:49:26 +0200

    crate output folder if does not exist

--- writeCsv.cpp
+++ writeCsv.cpp
@@ -1,12 +1,13 @@
-#include "writeCsv.h"
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <filesystem>
+#include "writeCsv.h"
 
 using namespace std;
 
 void writeCsv(int* histogram){
+    if (!filesystem::is_directory("../output") || !filesystem::exists("output"))
+        filesystem::create_directory("../output");
     ofstream fileIterations("../output/histogram.csv", ifstream::out);
     for (int i = 0; i < 256; i++ ){
         fileIterations << histogram[i] << "\n";
