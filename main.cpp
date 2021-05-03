07e26797ca59cca25710db38ad449281373a24fe
MarcoSolarino <marcosolarino@live.it>
2021-05-03 19:00:59 +0200

    - added tests for evaluation of execution time

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std::chrono;
 
 
 int main(int argc, char** argv) {
-    String imgName = argv[1];
+    String imgName = "img.jpg";
     Mat inputImg = imread("../input/" + imgName, 0);
     //imshow("Image before LBP", inputImg);
     auto start = chrono::high_resolution_clock::now();
@@ -16,8 +16,8 @@ int main(int argc, char** argv) {
     auto end = chrono::high_resolution_clock::now();
     auto ms_int = duration_cast<chrono::milliseconds>(end - start);
 
-    //imshow("Image after LBP", outputImg);
-    //waitKey(0);
+    imshow("Image after LBP", outputImg);
+    waitKey(0);
 
     return ms_int.count();
 }
